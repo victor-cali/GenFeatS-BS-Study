d8d921aae915ea654b6a1cb8ba4870a937877023
V. A. Canles L <victorcanlima@gmail.com>
2022-08-28 13:47:31 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Study of Genetic Feature Selection in Brain Signals',
+    author='Victor A. C. Lima',
+    license='',
+)
