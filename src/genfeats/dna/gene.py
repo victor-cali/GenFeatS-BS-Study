e0fce5ab463cefd9975c20a2a6949c3954fb1ec7
V. A. Canaeles L <victorcanlima@outlook.com>
2022-09-19 11:29:31 -0500

    Implemented gene and chromesome models

--- src/genfeats/dna/gene.py
+++ src/genfeats/dna/gene.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from typing import Any
 import json
 
-@dataclass(frozen=True)
+@dataclass(frozen=True, order=True)
 class Gene:
     
     func: str
@@ -12,13 +12,16 @@ class Gene:
     
     def __post_init__(self) -> None:
         super().__setattr__('func', str(self.func))
-        super().__setattr__('freq', tuple(tuple(band) for band in self.freq))
-        super().__setattr__('source', tuple(tuple(channel) for channel in self.source))
+        super().__setattr__('freq', tuple(sorted(tuple(band) for band in self.freq)))
+        super().__setattr__('source', tuple(sorted(self.source)))
         func_args = self.func_args.copy()
         for key, value in func_args.items():
             if isinstance(value, list):
                 func_args.update({key: tuple(value)})
-        super().__setattr__('func_args', tuple(sorted(func_args.items()))) 
+        super().__setattr__('func_args', tuple(sorted(func_args.items())))
+        
+    def  __str__(self) -> str:
+        return self.to_json()
     
     @classmethod
     def from_dict(cls, gene: dict):
@@ -27,4 +30,16 @@ class Gene:
     @classmethod
     def from_json(cls, gene: str):
         gene = json.loads(gene)
-        return cls(**gene)
+        return cls.from_dict(gene)
+
+    def to_dict(self) -> dict:
+        gene = {
+            'func': self.func,
+            'freq': self.freq,
+            'source': self.source,
+            'func_args': dict(self.func_args)
+        }
+        return gene
+
+    def to_json(self) -> str:
+        return json.dumps(self.to_dict())
