f2e8db00349cdd34759e9f5538bae260613439ae
V. A. Canaeles L <victorcanlima@outlook.com>
2022-09-24 18:10:41 -0500

    Updated gitignore

--- src/genfeats/tests/test_genotype_builder.py
+++ src/genfeats/tests/test_genotype_builder.py
@@ -1,1 +1,14 @@
+import pytest
+import json
+from src.genfeats.dna.gene import Gene
+from src.genfeats.dna.chromesome import Chromesome
 
+class TestGenotypeBuilder:
+
+    @pytest.fixture
+    def dict_gene(self) -> dict:
+        return None
+
+    
+    def test_gene(self, dict_gene):
+        pass
