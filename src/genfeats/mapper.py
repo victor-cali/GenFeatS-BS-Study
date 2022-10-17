446df55a8db5029d45b547f0e61e5a904bbc2e78
V. A. Canaeles L <victorcanlima@outlook.com>
2022-10-16 21:28:35 -0500

    Implemented Mapper

--- src/genfeats/mapper.py
+++ src/genfeats/mapper.py
@@ -1,4 +1,5 @@
 import ray
+from ray.util.multiprocessing import Pool
 import numpy as np
 from mne.epochs import BaseEpochs
 from typing import Union
@@ -6,52 +7,85 @@ from typing import Callable
 from src.genfeats.dna.gene import Gene
 from src.genfeats.dna.chromesome import Chromesome
 from inspect import getfullargspec, getmembers, isfunction
-from importlib.machinery import SourceFileLoader
+import importlib.util
+import sys
 
 class Mapper:
     
-    def __init__(self, features: str, epochs: BaseEpochs) -> None:
-        self.features = SourceFileLoader('features', features).load_module()
-        self.features = dict(getmembers(self.features, isfunction))
+    def __init__(self, features_file: str, chromesome_size: int, epochs: BaseEpochs) -> None:
+        self.pool = Pool()
         self.epochs = epochs
         self.sfreq = epochs.info['sfreq']
-    
+        self.features_handler = FeaturesHandler.remote(features_file)
+        self.features_args = ray.get(self.features_handler.get_features_args.remote())
+        
     def to_phenotype(self, dna: Union[Gene, Chromesome]) -> np.ndarray:
         if isinstance(dna, Gene):
-            gene = self.__prepare_mapping(dna)
-            feature = self.features[gene['feature']]
-            feature_parameters = gene['feature_parameters']
-            reference = map.remote(f=feature, kwargs=feature_parameters)
-            phenotype = ray.get(reference)
+            phenotype = self.__gene_to_phenotype(dna)
         elif isinstance(dna, Chromesome):
-            chromesome_size = len(dna)
-            phenotype = np.empty((len(epochs), chromesome_size))
-            references = []
-            for i in range(chromesome_size):
-                gene = self.__prepare_mapping(dna[i])
-                feature = self.features[gene['feature']]
-                feature_parameters = gene['feature_parameters']
-                references.append(map.remote(f=feature, kwargs=feature_parameters))
-            for i in range(chromesome_size):
-                phenotype[:, i] = ray.get(references[i])
+            phenotype = self.__chromesome_to_phenotype(dna)
         else:
             raise TypeError("Only Genes and Chromesomes can be maped to a Phenotype")
+        return phenotype
+
+    def __gene_to_phenotype(self, gene: Gene) -> np.ndarray:
+        gene = self.__prepare_mapping(gene)
+        reference = self.pool.starmap(
+            self.features_handler.map.remote,
+            [(gene['data'], gene['feature'], gene['feature_parameters'])]
+        )
+        phenotype = ray.get(reference[0])
+        return phenotype
+    
+    def __chromesome_to_phenotype(self, chromesome: Chromesome) -> np.ndarray:
+        mylist = []
+        n_genes = len(chromesome)
+        phenotype = np.empty((len(self.epochs), n_genes))
+        for i in range(n_genes):
+            gene = self.__prepare_mapping(chromesome[i])
+            mylist.append((gene['data'], gene['feature'], gene['feature_parameters']))
+        references = self.pool.starmap(
+            self.features_handler.map.remote,
+            mylist
+        )
+        for i in range(n_genes):
+            phenotype[:, i] = ray.get(references[i])
         return phenotype
     
     def __prepare_mapping(self, gene: Gene) -> dict:
         gene = gene.to_dict()
-        if 'freq_band' in set(getfullargspec(features['f1'])[0]):
+        if 'freq_band' in self.features_args[gene['feature']]:
             gene['feature_parameters']['freq_band'] = gene['freq_bands']
             data = gene['channels']
         else:
             channels = gene['channels']
             freq_bands = gene['freq_bands']
             data = [f'{ch}({fb[0]}-{fb[1]})' for ch in channels for fb in freq_bands]
-        if 'sfreq' in set(getfullargspec(features['f1'])[0]):
+        if 'sfreq' in self.features_args[gene['feature']]:
             gene['feature_parameters']['sfreq'] = self.sfreq
-        gene['feature_parameters']['data'] = self.epochs.get_data(picks=data)
+        gene['data'] = self.epochs.get_data(picks=data)
+        del gene['channels']
+        del gene['freq_bands']
         return gene
 
 @ray.remote
-def map(f: Callable, kwargs: dict):
-    return f(**kwargs)
+class FeaturesHandler():
+    def __init__(self, features_file: str) -> None:
+        module_name = 'gfsbs_features'
+        spec = importlib.util.spec_from_file_location('gfsbs_features', features_file)
+        module = importlib.util.module_from_spec(spec)
+        sys.modules['gfsbs_features'] = module
+        spec.loader.exec_module(module)
+        self.features = dict(getmembers(module, isfunction))
+        self.features_args = {key: getfullargspec(value)[0] for key, value in self.features.items()}
+        
+    def map(self, data, feature, kwargs):
+        n_epochs = data.shape[0]
+        phenotype = np.empty(n_epochs)
+        for i in range(n_epochs):
+            phenotype[i] = self.features[feature](data=data[i,:], **kwargs)
+        return phenotype
+
+    def get_features_args(self):
+        return self.features_args
+    
--- src/genfeats/tests/test_mapper.py
+++ src/genfeats/tests/test_mapper.py
@@ -3,18 +3,35 @@ import numpy as np
 from src.genfeats.mapper import Mapper
 from src.genfeats.dna.gene import Gene
 from src.genfeats.dna.chromesome import Chromesome
+from src.sighandling.sighandling import get_dataset_bbcic4_2b
+
 
 class TestMapper:
     
     @pytest.fixture
-    def mapper(self) -> Mapper:
-        return Mapper('D:/Dev/GenFeatS-BS-Study/resources/features.py')
+    def epochs(self):
+        new_names = dict()
+        filtered_epochs = list()
+        epochs = get_dataset_bbcic4_2b('D:/Dev/GenFeatS-BS-Study/data/external/segmented/S4_clean.mat')
+        for name in epochs.ch_names:
+            new_names.update({name: f'{name}(4-12)'})
+        subepochs = epochs.copy()
+        subepochs.filter(4, 12, method = 'iir', verbose = 50)
+        subepochs.rename_channels(new_names)
+        filtered_epochs.append(subepochs.copy())
+        epochs.add_channels(filtered_epochs, force_update_info=True)
+        return epochs
+    
+    @pytest.fixture
+    def mapper(self, epochs) -> Mapper:
+        features_file = 'D:/Dev/GenFeatS-BS-Study/resources/gfsbs_features.py'
+        return Mapper(features_file=features_file, chromesome_size=3, epochs=epochs)
     
     @pytest.fixture
     def genes(self) -> tuple[Gene]:
-        g1 = Gene('f1', ((4, 7), (12, 20)), ('A', 'B'), {'a': 1, 'b': 2})
-        g2 = Gene('f2', ((4, 7), (15, 20)), ('A', 'C'), {'a': 3, 'c': 4})
-        g3 = Gene('f2', ((4, 7), (20, 30)), ('B', 'C'), {'b': 3, 'd': 4})
+        g1 = Gene('kurtosis', [[4,12]], ['Cz', 'C3'], {})
+        g2 = Gene('variance', [[4,12]], ['Cz'], {})
+        g3 = Gene('skewness', [[4,12]], ['Cz'], {})
         return g1, g2, g3
     
     @pytest.fixture
